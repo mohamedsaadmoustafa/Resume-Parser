01be12f8e201179e014eea4e4473563f57be6071
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2022-11-02 03:31:38 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -11,8 +11,9 @@ import os
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Resume Parser")
 #st.image('images/header.png', width=800)
-st.text("Using this to see how well your resume is read by Application Tracking Systems (ATS) when applying to jobs.")
-
+st.text("See how well your resume is read by Application Tracking Systems (ATS)")
+st.text("when applying to jobs.")
+st.text("I love u beba :* <3")
 with st.form(key="Form :", clear_on_submit=True):
     datafile = st.file_uploader(label="Upload resume", type=["pdf", "docx", "txt"])
     Submit = st.form_submit_button(label='Submit')

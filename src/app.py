a65744f8ed46c8fd899e1cd67fc33fc95cf7b155
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2022-11-02 02:11:25 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ import streamlit as st
 from pipeline import ResumeParser
 from resume_text import ResumeText
 from visualization.cloudwords import show_words
+import os
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Resume Parser")
@@ -15,18 +16,23 @@ with st.form(key="Form :", clear_on_submit=True):
     datafile = st.file_uploader(label="Upload resume", type=["pdf", "docx", "txt"])
     Submit = st.form_submit_button(label='Submit')
 
+def save_uploadedfile(uploadedfile):
+    # Save uploaded file to 'upload' folder.
+    save_folder = 'upload/'
+    save_path = Path(save_folder, uploadedfile.name)
+    with open(save_path, mode='wb') as w:
+        w.write(uploadedfile.getvalue())
+        w.write(uploadedfile.getbuffer())
+    return st.success(f"Saved File:{uploadedfile.name} to uploads")
+
 if Submit:
     if datafile:
         # Create a text element and let the reader know the data is loading.
         data_load_state = st.info('Uploading Reseme')
-        # Save uploaded file to 'F:/tmp' folder.
-        save_folder = 'upload/'
-        save_path = Path(save_folder, datafile.name)
-        #with open(save_path, mode='wb') as w:
-        #    w.write(datafile.getvalue())
-            
-        file_details = {"FileName":datafile.name,"FileType":datafile.type}
-        text = ResumeText(file_details).text
+        # save_uploadedfile(datafile)
+        # file_details = {"FileName": datafile.name, "FileType": datafile.type}
+
+        text = ResumeText(datafile, datafile.type).text
         data_load_state.success('Extracting text from reseme')
 
         col1, col2 = st.columns(2)
